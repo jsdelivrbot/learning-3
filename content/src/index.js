fc13d3cb98fb5aa96fce9181cff5730367785454
Marius <marius.seba@gmail.com>
2016-08-23 17:05:39 +0300

    first commit

---
+++ content/src/index.js
@@ -0,0 +1,12 @@
+// imports
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// create a new component
+
+ const App = () => {
+ 	return <div> Hi! </div>;
+ }
+
+// take this component's generated HTML and put it on the page(in the DOM)
+ReactDOM.render(<App />, document.querySelector('.container'));
