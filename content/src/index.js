55eca6e3f12947bee5086073761558d76fb4595e
Marius <marius.seba@gmail.com>
2016-08-25 11:53:01 +0300

    plm

--- content/src/index.js
+++ content/src/index.js
@@ -2,10 +2,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// components
+import SearchBar from './components/search_bar';
+
+// youtube api key
+const API_KEY = 'AIzaSyCYJwuGbbUYHwzy2GVd3X_Dpa_neUo6QMk';
+
 // create a new component
 
  const App = () => {
- 	return <div> Hi! </div>;
+ 	return( 
+ 	<div>
+ 		<SearchBar />
+ 	</div>
+ 	);
  }
 
 // take this component's generated HTML and put it on the page(in the DOM)
