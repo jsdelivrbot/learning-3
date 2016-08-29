1b35eadf4cbe60caa876d7ebc3576d18c09fd04f
Marius <marius.seba@gmail.com>
2016-08-29 17:30:23 +0300

    update 29 august

--- content/src/index.js
+++ content/src/index.js
@@ -1,20 +1,33 @@
 // imports
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-
-// components
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 // youtube api key
 const API_KEY = 'AIzaSyCYJwuGbbUYHwzy2GVd3X_Dpa_neUo6QMk';
 
 // create a new component
- const App = () => {
- 	return( 
- 	<div>
- 		<SearchBar />
- 	</div>
- 	);
+ class App extends Component {
+ 	constructor(props) {
+ 		super(props);
+
+ 		// state
+ 		this.state = { videos: [] };
+
+ 		// youtube videos
+		YTSearch({key: API_KEY, term: 'pilav'}, (videos) => {
+			this.setState({videos});
+		});
+ 	}
+
+ 	render() {
+	 	return( 
+	 	<div>
+	 		<SearchBar />
+	 	</div>
+	 	);
+ 	}
  }
 
 // take this component's generated HTML and put it on the page(in the DOM)
